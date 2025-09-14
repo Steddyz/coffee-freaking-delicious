d8f2fdfbf8e14a1e7ad8d42cca7436bd10b46f2b
Steddy <138579152+Steddyz@users.noreply.github.com>
2025-09-14 15:30:03 +0300

    add animations with gsap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { ScrollTrigger } from "gsap/all";
 import Navbar from "./components/Navbar";
 import HeroSection from "./sections/HeroSection";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <HeroSection />
+      <div className="h-dvh border border-red-500" />
     </main>
   );
 };
